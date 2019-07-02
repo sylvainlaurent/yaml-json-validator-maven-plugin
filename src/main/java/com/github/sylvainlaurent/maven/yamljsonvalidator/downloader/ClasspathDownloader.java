76a6e4beeccf6ab0a5492f618919fcd0fec8c5af
Stefan Djurasic <stefan.djurasic@payback.net>
2019-07-02 15:52:10 +0200

    added more information about how to use classpath in json schemas.

--- src/main/java/com/github/sylvainlaurent/maven/yamljsonvalidator/downloader/ClasspathDownloader.java
+++ src/main/java/com/github/sylvainlaurent/maven/yamljsonvalidator/downloader/ClasspathDownloader.java
@@ -5,6 +5,15 @@ import java.net.URI;
 
 import com.github.fge.jsonschema.core.load.download.URIDownloader;
 
+/**
+ * Enables loading ref's in schemas from classpath. E.g.: <br/>
+ * <code>"$ref": "classpath:schema/includes/a-include.json"</code> <br/>
+ * This only works, if the schemas are really on the classpath. <br/>
+ * Adding them only via project dependencies is not enough. They need to be added
+ * to the plugin dependencies so they will be available through the plugin classloader.<br/>
+ * To make this all work, schemas defined by the configuration element <code>&#060;jsonSchema&#062;someSchema.json&#060;/jsonSchema&#062;</code>
+ * will also be searched on the classpath if not found on the filesystem.
+*/
 public class ClasspathDownloader implements URIDownloader {
 
     @Override
