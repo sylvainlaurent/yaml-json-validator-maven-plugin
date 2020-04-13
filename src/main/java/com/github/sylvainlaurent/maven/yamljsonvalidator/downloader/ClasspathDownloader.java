8afee866f9c31414b45caa70bb668425e0a1b64d
Sylvain LAURENT <slaurent@apache.org>
2020-04-13 18:38:41 +0200

    Fixed javadoc

--- src/main/java/com/github/sylvainlaurent/maven/yamljsonvalidator/downloader/ClasspathDownloader.java
+++ src/main/java/com/github/sylvainlaurent/maven/yamljsonvalidator/downloader/ClasspathDownloader.java
@@ -6,13 +6,13 @@ import java.net.URI;
 import com.github.fge.jsonschema.core.load.download.URIDownloader;
 
 /**
- * Enables loading ref's in schemas from classpath. E.g.: <br/>
- * <code>"$ref": "classpath:schema/includes/a-include.json"</code> <br/>
- * This only works, if the schemas are really on the classpath. <br/>
+ * <p>Enables loading ref's in schemas from classpath. E.g.:
+ * <code>"$ref": "classpath:schema/includes/a-include.json"</code> </p>
+ * <p>This only works, if the schemas are really on the classpath.
  * Adding them only via project dependencies is not enough. They need to be added
- * to the plugin dependencies so they will be available through the plugin classloader.<br/>
- * To make this all work, schemas defined by the configuration element <code>&#060;jsonSchema&#062;someSchema.json&#060;/jsonSchema&#062;</code>
- * will also be searched on the classpath if not found on the filesystem.
+ * to the plugin dependencies so they will be available through the plugin classloader.</p>
+ * <p>To make this all work, schemas defined by the configuration element <code>&#060;jsonSchema&#062;someSchema.json&#060;/jsonSchema&#062;</code>
+ * will also be searched on the classpath if not found on the filesystem.</p>
 */
 public class ClasspathDownloader implements URIDownloader {
 
